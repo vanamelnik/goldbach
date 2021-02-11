2ff1af770cadfa8f2f979e16ac28d00a66cd263b
Ivan Melnik <jmvmiller@gmail.com>
2021-02-11 17:35:58 +0300

    Implemented primeGen function in abGen

--- abGen/abGen.go
+++ abGen/abGen.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // var (
 // 	// Primes - slice of prime numbers >= 5
@@ -19,17 +22,61 @@ type GoldbachTables struct {
 
 func main() {
 	fmt.Println("abGen - генерация номеров A- и B- простых чисел")
-	const max = 1000
+	const max = 100
 	var gt GoldbachTables
-
+	gt.primeGen(max)
+	fmt.Printf("Простые числа до %d:\n", max)
+	fmt.Println(gt.Primes)
+	fmt.Println("\nАльфа-числа, номера:")
+	fmt.Println(gt.A)
+	fmt.Println("\nБета-числа, номера:")
+	fmt.Println(gt.B)
 }
 
+// TODO: addition comments!
 func (gt *GoldbachTables) primeGen(max uint64) {
-	for i := len(gt.Primes); i < max; i++ {
+	const (
+		isAlpha = 4
+		isBeta  = 2
+	)
+	var addition uint64 = isAlpha
+	var n uint64
+	if len(gt.Primes) > 0 {
+		n = gt.Primes[len(gt.Primes)-1] // n is the last known prime or zero
+		if (gt.Primes[n-1]+1)%6 == 0 {  // if the last prime in table is an alpha-prime,
+			addition = isBeta // select 'beta' addition for the next check
+		}
+	}
 
+	for n < max {
+		n += addition // add to the last known prime 2 or 4
+		if gt.isPrime(n) {
+			gt.Primes = append(gt.Primes, n)
+			if addition == isAlpha {
+				gt.A = append(gt.A, (n+1)/6) // new alpha-prime (6k-1), add k to table A
+			} else {
+				gt.B = append(gt.B, (n-1)/6) // new beta-prime (6k+1), add k to table B
+			}
+			if addition == isAlpha {
+				addition = isBeta
+			} else {
+				addition = isAlpha
+			}
+		}
 	}
 }
-
 func (gt *GoldbachTables) isPrime(n uint64) bool {
-	return false
+	if len(gt.Primes) == 0 {
+		if n == 5 {
+			gt.Primes = append(gt.Primes, 5)
+			return true
+		}
+		return false
+	}
+	for i := 0; gt.Primes[i] <= uint64(math.Sqrt(float64(n))); i++ {
+		if n%gt.Primes[i] == 0 {
+			return false
+		}
+	}
+	return true
 }
